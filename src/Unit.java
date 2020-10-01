37b7373cda62c105b0b3afae54c86c87ac710285
michellebazan <24michelle.b@gmail.com>
2020-09-30 17:25:49 -0700

    game(wip)

--- src/Unit.java
+++ src/Unit.java
@@ -1,32 +1,54 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
 public class Unit extends GameObject{
+//variables
 	//stats
 	int HP;
 	int Atk;
 	int Spd;
 	int Def;
 	int Res;
-	
-	Affinity element; //can check what element = to
+	int Mag;
+	int Lck;
+	Affinity element; //can check what element is equal to
+	final static int BASEHITRATE = 85;
 	
 	public static BufferedImage image;
 	public static boolean needImage = true;
 	public static boolean gotImage = false;	
 	
-	//each unit has diff. stats
-	
-	Unit(int HP, int Atk, int Spd, int Def, int Res, int x, int y, int width, int height){
+	//constructor
+	Unit(int HP, int Atk, int Spd, int Def, int Res, int Mag, int Lck, Affinity element, int x, int y, int width, int height){
 		//The super keyword refers to superclass (parent) objects. It is used to call superclass methods,
 		//and to access the superclass constructor. The most common use of the super keyword is to eliminate
 		//the confusion between superclasses and subclasses that have methods with the same name
 		super(x, y, width, height); 
-		
-		
+	}
+	
+	void attack(Unit unit) {
+		//speed
+		int numAttacks = 1;
+		if (Spd >= unit.Spd+4) {
+			//twice the attacks
+			numAttacks = 2;
+		}
+		//hit rate + luck + ???
+		for (int i = 0; i < numAttacks; i++) {
+			//
+			int trueHitRate = BASEHITRATE + Lck - unit.Lck;
+			//random
+			Random rand = new Random();
+			
+		}
+	}
+	
+	void update(){
+		//may or may not be needed here, depends on project development
 	}
 	
 	void draw(Graphics g) {
