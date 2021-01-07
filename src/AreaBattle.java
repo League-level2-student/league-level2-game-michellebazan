246e0b76959cd83f263cd44eb815fad745973189
michellebazan <24michelle.b@gmail.com>
2021-01-06 17:23:44 -0800

    game wip

--- src/AreaBattle.java
+++ src/AreaBattle.java
@@ -1,176 +1,184 @@
-import  java.util.Random;
+import java.util.Random;
 
-import  javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
 
-public  class  AreaBattle  {  //i  think  it  was  supposed  to  be  called  ArenaBattle  but  arena  looks  like  a  fake  word  so  the  name  is  not  going  to  change
-	public  static  void  main(String[]  args)  {
-		//after  testing,  program  another  action  (like  healing)  to  unit  support                 
-		
-		//Unit(int  HP,  int  Atk,  int  Spd,  int  Def,  int  Res,  int  Mag,  int  Lck,  Affinity  element,  int  x,  int  y,  int  width,  int  height)
-		Unit  Fred  =  new  Unit(30,  10,  8,  5,  2,  2,  6,  Affinity.FIRE,  20,  20,  20,  20,  "Fred");  //starting  unit
-		Unit  Runrun  =  new  Unit(28,  4,  7,  3,  7,  9,  9,  Affinity.AIR,  20,  21,  20,  20,  "Runrun");  //starting  unit
-		
-		//other  units
-		Unit  Night  =  new  Unit(27,  7,  7,  4,  8,  12,  4,  Affinity.WATER,  20,  21,  20,  20,  "Night");
-		Unit  Child  =  new  Unit(26,  10,  9,  5,  2,  5,  5,  Affinity.AIR,  20,  21,  20,  20,  "Child");
-		Unit  Tilly  =  new  Unit(28,  8,  7,  6,  6,  7,  5,  Affinity.EARTH,  20,  21,  20,  20,  "Tilly");
-		Unit  Fraser  =  new  Unit(25,  9,  10,  5,  2,  3,  7,  Affinity.FIRE,  20,  21,  20,  20,  "Fraser");
-		int  round  =  0;
-		//prologue
-		int  prologue  =  JOptionPane.showOptionDialog(null,  "Welcome  to  the  battle  area.  What  would  you  like  to  do",  "At  the  doorway",  0,  -1,  null,  new  String  []  {"Read  Instructions",  "Start"},  0);
-		if  (prologue  ==  0){
-			//instructions  here
-			JOptionPane.showMessageDialog(null,  "There  are  two  duelers,  and  you  will  decide  the  fate  of  them.");
-			JOptionPane.showMessageDialog(null,  "There  will  be  different  attack  options,  depending  on  the  background  of  the  dueler.  ");
-			JOptionPane.showMessageDialog(null,  "Oh  and  background  means  that  each  dueler  has  their  own  elemental  affinity  and  abiltity  (magic  vs  physical  attack).  ");
-			JOptionPane.showMessageDialog(null,  "Anyways  when  a  dueler's  HP  reaches  zero,  the  game  will  end.  You  may  choose  to  play  another  round  or  forever  leave  the  area.  ");
-			
-			prologue=1;
-			
+public class AreaBattle { // i think it was supposed to be called ArenaBattle but arena looks like a fake
+							// word so the name is not going to change
+	public static void main(String[] args) {
+		// after testing, program another action (like healing) to unit support
+
+		// Unit(int HP, int Atk, int Spd, int Def, int Res, int Mag, int Lck, Affinity
+		// element, int x, int y, int width, int height)
+		Unit Fred = new Unit(30, 10, 8, 5, 2, 2, 6, Affinity.FIRE, 20, 20, 20, 20, "Fred"); // starting unit
+		Unit Runrun = new Unit(28, 4, 7, 3, 7, 9, 9, Affinity.AIR, 20, 21, 20, 20, "Runrun"); // starting unit
+
+		// other units
+		Unit Night = new Unit(27, 7, 7, 4, 8, 12, 4, Affinity.WATER, 20, 21, 20, 20, "Night");
+		Unit Child = new Unit(26, 10, 9, 5, 2, 5, 5, Affinity.AIR, 20, 21, 20, 20, "Child");
+		Unit Tilly = new Unit(28, 8, 7, 6, 6, 7, 5, Affinity.EARTH, 20, 21, 20, 20, "Tilly");
+		Unit Fraser = new Unit(25, 9, 10, 5, 2, 3, 7, Affinity.FIRE, 20, 21, 20, 20, "Fraser");
+		int round = 0;
+		// prologue
+		int prologue = JOptionPane.showOptionDialog(null,
+				"Welcome  to  the  battle  area.  What  would  you  like  to  do", "At  the  doorway", 0, -1, null,
+				new String[] { "Read  Instructions", "Start" }, 0);
+		if (prologue == 0) {
+			// instructions here
+			JOptionPane.showMessageDialog(null,
+					"There  are  two  duelers,  and  you  will  decide  the  fate  of  them.");
+			JOptionPane.showMessageDialog(null,
+					"There  will  be  different  attack  options,  depending  on  the  background  of  the  dueler.  ");
+			JOptionPane.showMessageDialog(null,
+					"Oh  and  background  means  that  each  dueler  has  their  own  elemental  affinity  and  abiltity  (magic  vs  physical  attack).  ");
+			JOptionPane.showMessageDialog(null,
+					"Anyways  when  a  dueler's  HP  reaches  zero,  the  game  will  end.  You  may  choose  to  play  another  round  or  forever  leave  the  area.  ");
+
+			prologue = 1;
+
 		}
-		
-		Random  ballotnum  =  new  Random();
-		Random  num22  =  new  Random();
-	        //  array  to  include  units  +  including  random  values
-		Unit[]  fighters  =  {Fred,  Runrun,  Night,  Child,  Tilly,  Fraser};
-		Unit  Unit1  =  fighters[ballotnum.nextInt(7)];
-		Unit  Unit2  =  fighters[num22.nextInt(7)];
-		
-		while  (Unit1.name  ==  Unit2.name)  {
-			//makes  sure  that  units  will  not  be  the  same
-			Random  ballotnum22  =  new  Random();
-			Random  num2222  =  new  Random();
-	
-			Unit1  =  fighters[ballotnum22.nextInt(7)];
-			Unit2  =  fighters[num2222.nextInt(7)];
+
+		// one random can be used for different arrays/purposes. it does not hold random
+		// numbers, it generates them.
+		// ctrl shift f is to format the code
+		Random ballotnum = new Random();
+
+		// array to include units + including random values
+		Unit[] fighters = { Fred, Runrun, Night, Child, Tilly, Fraser };
+		Unit Unit1 = fighters[ballotnum.nextInt(fighters.length)]; // can add other fighters
+		Unit Unit2 = fighters[ballotnum.nextInt(fighters.length)];
+
+		while (Unit1.name == Unit2.name) {
+			// makes sure that units will not be the same
+
+			Unit1 = fighters[ballotnum.nextInt(fighters.length)];
+			Unit2 = fighters[ballotnum.nextInt(fighters.length)];
 		}
-		
-		JOptionPane.showMessageDialog(null,  "So  to  start  off,  there  are  already  two  chosen  fighters;  they  are  called  "  +  Unit1.name  +  "  and  "  +  Unit2.name  +  ".  ");
-		
-		//repeats  until  player  wishes  to  leave  arena
-		//round  =  0  ,,,  first  loop  it  begins  at  0
-		//bug  starts(?)  here  *******************************************************************************************************
-		//thoughts  on  bug - - - - - - - - - - - - - - - - - - - - - - - -
-				//it  forcefully  breaks  the  game  after  2-3  rounds
-				//very  inconsistent  in  the  expected  outcome  of  clicking  buttons
-					//ex/  going  to  the  next  round  and  repeats  who  is  fighting  multiple  times
-					//ex/  i  play  it  through  one  time  and  it  closes  after  the  first  round.  i  play  it  again  and  
-					//it  lasts  past  the  first  round  (with  the  same  code)
-			//bug  can  become  even  worse,  breaks  after  the  first  round  ///  became  worse  after  I  changed  line  60  to  round  >  0.
-			//the  bug  is  involved  with  integer  round, not with attackoptions or anything else
-		
-		while  (round  >  -1)  {//*************************************************************************************************************************************************
-			
-			if(round>1)  {  //rounds  that  are  not  the  very  first  one  
-				Random  ballotnum2  =  new  Random();
-				Random  num222  =  new  Random();
-		
-				Unit1  =  fighters[ballotnum2.nextInt(7)];
-				Unit2  =  fighters[num222.nextInt(7)];
-				
-				while  (Unit1.name  ==  Unit2.name)  {
-					//makes  sure  that  units  will  not  be  the  same
-					Random  ballotnum22  =  new  Random();
-					Random  num2222  =  new  Random();
-			
-					Unit1  =  fighters[ballotnum22.nextInt(7)];
-					Unit2  =  fighters[num2222.nextInt(7)];
+
+		JOptionPane.showMessageDialog(null,
+				"So  to  start  off,  there  are  already  two  chosen  fighters;  they  are  called  " + Unit1.name
+						+ "  and  " + Unit2.name + ".  ");
+
+		// current bugs found by tester C 1/6/2020
+		// repeats itself
+		// ex/clicking the mag button multiple times in order for it to work
+		// ex/scene where fighters are introduced repeats (after the first round)
+		// Can skip scenes
+		// ex/announcing the hp of a unit
+		// after one run, it ended after 4 rounds. then on another run, it ended again.
+		while (round > -1) {// *************************************************************************************************************************************************
+
+			if (round > 1) { // rounds that are not the very first one
+
+				Unit1 = fighters[ballotnum.nextInt(fighters.length)];
+				Unit2 = fighters[ballotnum.nextInt(fighters.length)];
+
+				while (Unit1.name == Unit2.name) {
+
+					Unit1 = fighters[ballotnum.nextInt(fighters.length)];
+					Unit2 = fighters[ballotnum.nextInt(fighters.length)];
+				}
+
 			}
-			
+
+			if (round >= 1) {
+				JOptionPane.showMessageDialog(null, "For  this  round,  there  are  two  fighters;  they  are  called  "
+						+ Unit1.name + "  and  " + Unit2.name + ".  ");
 			}
-			
-		if  (round  >=1)  {
-			JOptionPane.showMessageDialog(null,  "For  this  round,  there  are  two  fighters;  they  are  called  "  +  Unit1.name  +  "  and  "  +  Unit2.name  +  ".  ");
-		}
-		
-		//below is not involved with bug
-		while(Unit1.HP  >=  0  &&  Unit2.HP  >=  0)  {
-			
-			int  attackoption  =  JOptionPane.showOptionDialog(null,  "Choose  an  attack",  Unit1.name,  0,  -1,  null,  new  String  []  {"ATK",  "MAG"},  0);
-			//Unit1's  attack  options
-			if  (  attackoption  ==  0)  {
-				//atk
-				Unit1.attack(Unit2,  false);
-			}  else  if  (attackoption  ==  1)  {
-				//mag
-				//Unit1.attack(Unit2,  true);
-				if(Unit1.Mag  >  5)  {
-					
-					attackoption  =  JOptionPane.showOptionDialog(null,  "Choose  an  attack",  Unit1.name,  0,  -1,  null,  new  String  []  {"HL",  "MAG"},  0);
-					if  (attackoption  ==  0)  {
-						Unit1.heal(Unit1,  true);
-					}  else  if  (attackoption  ==  1)  {
-						Unit1.attack(Unit2,  true);
+
+			// attack while loop
+			while (Unit1.HP > 0 && Unit2.HP > 0) {
+
+				int attackoption = JOptionPane.showOptionDialog(null, "Choose  an  attack", Unit1.name, 0, -1, null,
+						new String[] { "ATK", "MAG" }, 0);
+				// Unit1's attack options
+				if (attackoption == 0) {
+					// atk
+					Unit1.attack(Unit2, false);
+				} else if (attackoption == 1) {
+					// mag
+					// Unit1.attack(Unit2, true);
+					if (Unit1.Mag > 5) {
+
+						attackoption = JOptionPane.showOptionDialog(null, "Choose  an  attack", Unit1.name, 0, -1, null,
+								new String[] { "HL", "MAG" }, 0);
+						if (attackoption == 0) {
+							Unit1.heal(Unit1, true);
+						} else if (attackoption == 1) {
+							Unit1.attack(Unit2, true);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null,
+								Unit1.name + "  does  not  have  magic  so  nothing  happens...  ");
 					}
+
 				}
-				else  {
-					JOptionPane.showMessageDialog(null,  Unit1.name  +  "  does  not  have  magic  so  nothing  happens...  ");
+				if (Unit2.HP <= 0) {
+					JOptionPane.showMessageDialog(null, Unit1.name + "  won.");
+					// round++;
+					break;
+
 				}
-				
-				
+
+				attackoption = JOptionPane.showOptionDialog(null, "Choose  an  attack", Unit2.name, 0, -1, null,
+						new String[] { "ATK", "MAG" }, 0);
+				// Unit2's attack options
+				if (attackoption == 0) {
+					// atk
+					Unit2.attack(Unit1, false);
+				} else if (attackoption == 1) {
+					// mag
+					// Unit2.attack(Unit1, true);
+
+					if (Unit2.Mag > 5) {
+
+						attackoption = JOptionPane.showOptionDialog(null, "Choose  an  attack", Unit2.name, 0, -1, null,
+								new String[] { "HL", "MAG" }, 0);
+						if (attackoption == 0) {
+							Unit2.heal(Unit2, true);
+						} else if (attackoption == 1) {
+							Unit2.attack(Unit1, true);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null,
+								Unit2.name + "  does  not  have  magic  so  nothing  happens...  ");
+					}
+
+				}
+
+				if (Unit1.HP <= 0) {
+					JOptionPane.showMessageDialog(null, Unit2.name + "  won.");
+					// round++;
+					break;
+				}
+
 			}
-			if  (Unit2.HP  <=  0)  {
-				JOptionPane.showMessageDialog(null,  Unit1.name  +  "  won.");
-				//round++;
+
+			
+			int escape = JOptionPane.showOptionDialog(null, "Do  you  want  to  continue  in  the  battle  arena?",
+					"arena", 0, 1, null, new String[] { "Yes", "No" }, 0);
+			if (escape == 1) {
+				JOptionPane.showMessageDialog(null,
+						"You  leave  the  battle  arena  and  took  a  deep  breath.  The  air  tastes  crunchy  like  a  snicker  bar.");
+				JOptionPane.showMessageDialog(null,
+						"You  look  up  and  see  a  bird  pass  by.  You  realize  you  are  free  from  eternal  hell.");
 				break;
-			
 			}
-			
-			attackoption  =  JOptionPane.showOptionDialog(null,  "Choose  an  attack",    Unit2.name,  0,  -1,  null,  new  String  []  {"ATK",  "MAG"},  0);
-			//Unit2's  attack  options
-			if  (attackoption  ==  0)  {
-				//atk
-				Unit2.attack(Unit1,  false);
-			}  else  if  (attackoption  ==  1)  {
-				//mag
-				//Unit2.attack(Unit1,  true);
-				
-				if  (Unit2.Mag  >  5)  {
-					
-					attackoption  =  JOptionPane.showOptionDialog(null,  "Choose  an  attack",  Unit2.name,  0,  -1,  null,  new  String  []  {"HL",  "MAG"},  0);
-					if  (attackoption  ==  0)  {
-						Unit2.heal(Unit2,  true);
-					}  else  if  (attackoption  ==  1)  {
-						Unit2.attack(Unit1,  true);
-					}
-				}
-				else  {
-					JOptionPane.showMessageDialog(null,  Unit2.name  +  "  does  not  have  magic  so  nothing  happens...  ");
-				}
-				
+			round++; // increasing round to at least 1
+
+			if (round == 10) {
+				System.out.println("...");
+			} else if (round == 20) {
+				System.out.println("Don't  you  get  tired  of  fighting?");
+			} else if (round == 30) {
+				System.out.println("Have  you  thought  about  what  outside  is  like?");
+			} else if (round == 40) {
+				System.out.println("I  recommend  leaving  soon.");
+			} else if (round == 50) {
+				System.out.println("I  warned  you");
+				break;
 			}
-			
-		if  (Unit2.HP  <=  0)  {
-			JOptionPane.showMessageDialog(null,  Unit2.name  +  "  won.");
-			//round++;
-			break;
-		}
-		
-		}
-		
-		int  escape  =  JOptionPane.showOptionDialog(null,  "Do  you  want  to  continue  in  the  battle  arena?",  "arena",  0,  1,  null,  new  String  []  {"Yes",  "No"},  0);;
-		if  (escape  ==  1)  {
-			JOptionPane.showMessageDialog(null,  "You  leave  the  battle  arena  and  took  a  deep  breath.  The  air  tastes  crunchy  like  a  snicker  bar.");
-			JOptionPane.showMessageDialog(null,  "You  look  up  and  see  a  bird  pass  by.  You  realize  you  are  free  from  eternal  hell.");
-			break;
-		}  
-		round++; //increasing round to at least 1
-		
-		//below  does  not  affect  bug -- its just extra and i cant even test it since the bug stops me
-		if  (round==10)  {
-			System.out.println("...");
-		}  else  if  (round  ==20)  {
-			System.out.println("Don't  you  get  tired  of  fighting?");
-		}  else  if  (round  ==  30)  {
-			System.out.println("Have  you  thought  about  what  outside  is  like?");
-		}  else  if(round  ==  40)  {
-			System.out.println("I  recommend  leaving  soon.");
-		}else  if(round==50)  {
-			System.out.println("I  warned  you");
-			break;
-		}
-		
-		}//////////*****************************************************************************************************************************************
-		
+
+		} ////////// *****************************************************************************************************************************************
+
 	}
 }
--- src/Unit.java
+++ src/Unit.java
@@ -6,9 +6,9 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
-public class Unit extends GameObject{
+public class Unit extends GameObject {
 //variables
-	//stats
+	// stats
 	String name;
 	int HP;
 	int Atk;
@@ -17,20 +17,24 @@ public class Unit extends GameObject{
 	int Res;
 	int Mag;
 	int Lck;
-	Affinity element; //can check what element is equal to
+	Affinity element; // can check what element is equal to
 	final static int BASEHITRATE = 85;
-	//checks for images
+	// checks for images
 	public static BufferedImage image;
 	public static boolean needImage = true;
-	public static boolean gotImage = false;	
-	
-	//constructor
-	Unit(int HP, int Atk, int Spd, int Def, int Res, int Mag, int Lck, Affinity element, int x, int y, int width, int height, String Name){
-		//The super keyword refers to superclass (parent) objects. It is used to call superclass methods,
-		//and to access the superclass constructor. The most common use of the super keyword is to eliminate
-		//the confusion between superclasses and subclasses that have methods with the same name
-		super(x, y, width, height); 
-		//right - value passed in, left - value we want to save in the unit
+	public static boolean gotImage = false;
+
+	// constructor
+	Unit(int HP, int Atk, int Spd, int Def, int Res, int Mag, int Lck, Affinity element, int x, int y, int width,
+			int height, String Name) {
+		// The super keyword refers to superclass (parent) objects. It is used to call
+		// superclass methods,
+		// and to access the superclass constructor. The most common use of the super
+		// keyword is to eliminate
+		// the confusion between superclasses and subclasses that have methods with the
+		// same name
+		super(x, y, width, height);
+		// right - value passed in, left - value we want to save in the unit
 		this.HP = HP;
 		this.Atk = Atk;
 		this.Spd = Spd;
@@ -40,21 +44,23 @@ public class Unit extends GameObject{
 		this.Lck = Lck;
 		this.element = element;
 		this.name = Name;
-		
+
 	}
-	
+
 	void heal(Unit unit, boolean isMagic) {
 		Random randd = new Random();
-		//sets num of heals of luck of unit
+		// sets num of heals of luck of unit
 		int numHeals = 1;
-		//checks if our unit has any heals
+		// checks if our unit has any heals
 		while (numHeals > 0) {
-			//check if our unit has magic
-			if (isMagic == true) { //100 success rate but num of hp is random
-				//need to fix the equation for healing self if the unit has magic, check is it needs to be unit.HP (etc.)
-				int randnumm = randd.nextInt(HP); // figure out if you want to limit the heal option or not // can change it so at least half of the total hp is return through heal
-				//like if hp is 10 then it will return at least 5.
-				if (randnumm <5) {
+			// check if our unit has magic
+			if (isMagic == true) { // 100 success rate but num of hp is random
+				// need to fix the equation for healing self if the unit has magic, check is it
+				// needs to be unit.HP (etc.)
+				int randnumm = randd.nextInt(HP); // figure out if you want to limit the heal option or not // can
+													// change it so at least half of the total hp is return through heal
+				// like if hp is 10 then it will return at least 5.
+				if (randnumm < 5) {
 					randnumm = randnumm + 10;
 				}
 				if (randnumm > 25) {
@@ -62,42 +68,44 @@ public class Unit extends GameObject{
 				}
 				HP = HP + randnumm;
 				numHeals = numHeals - 1;
-				//message pop up
-				JOptionPane.showMessageDialog(null, unit.name + " health has increased by " + randnumm + " putting them at a total of " + HP);
+				// message pop up
+				JOptionPane.showMessageDialog(null,
+						unit.name + " health has increased by " + randnumm + " putting them at a total of " + HP);
 			}
-			
-		}		
+
+		}
 		numHeals++;
 	}
-	
+
 	void attack(Unit unit, boolean isMagic) {
-		//speed
+		// speed
 		int numAttacks = 1;
-		if (Spd >= unit.Spd+4) {
-			//twice the attacks
+		if (Spd >= unit.Spd + 4) {
+			// twice the attacks
 			numAttacks = 2;
 		}
-		//hit rate + luck
+		// hit rate + luck
 		for (int i = 0; i < numAttacks; i++) {
-			//battle of luck
+			// battle of luck
 			int trueHitRate = BASEHITRATE + Lck - unit.Lck;
-			//random based on hit rate --> succeeds/fails
-			//use  random to get a number between 1 and 100. 
-			//if we get a number that's under true hit rate --> we can deal damage to the target unit using attack against their defense
+			// random based on hit rate --> succeeds/fails
+			// use random to get a number between 1 and 100.
+			// if we get a number that's under true hit rate --> we can deal damage to the
+			// target unit using attack against their defense
 			Random rand = new Random();
 
-			//for each type of attack allowed based on spd
-			while (numAttacks > 0){
+			// for each type of attack allowed based on spd
+			while (numAttacks > 0) {
 				int randnum = rand.nextInt(101);
 				if (randnum < trueHitRate) {
-					//any attack - their defense = damage to their HP
-					if(isMagic == true) {
+					// any attack - their defense = damage to their HP
+					if (isMagic == true) {
 						Double damagetaken = Mag * getAffinityModifier(unit.element) - unit.Res;
 						if (damagetaken < 0) {
 							damagetaken = 0.0;
 						}
-						unit.HP = (int) (unit.HP - damagetaken); 
-						//name is our unit, unit is other
+						unit.HP = (int) (unit.HP - damagetaken);
+						// name is our unit, unit is other
 					} else if (isMagic == false) {
 						Double damagetaken = Atk * getAffinityModifier(unit.element) - unit.Def;
 						if (damagetaken < 0) {
@@ -105,125 +113,124 @@ public class Unit extends GameObject{
 						}
 						unit.HP = (int) (unit.HP - damagetaken);
 					}
-					JOptionPane.showMessageDialog(null, unit.name + "'s health is at " + unit.HP);
-					
+					JOptionPane.showMessageDialog(null, unit.name + "'s health is at " + unit.HP); // here it announces
+																									// the health of the
+																									// unit
+
 				}
-			numAttacks = numAttacks - 1;
-			//if it passes --> atk per numAttacks
-			//if fails --> nothing, turn passed
+				numAttacks = numAttacks - 1;
+				// if it passes --> atk per numAttacks
+				// if fails --> nothing, turn passed
 			}
-			
-			
+
 		}
 	}
-	
+
 	// air, fire, water, earth + multiplier on strength: .5, 1, 2,
 	double getAffinityModifier(Affinity unitAffinity) {
-		//checks if main element is air
-		if(element == Affinity.AIR) {
-			//double damage against earth
+		// checks if main element is air
+		if (element == Affinity.AIR) {
+			// double damage against earth
 			if (unitAffinity == Affinity.EARTH) {
-				//returns damage
+				// returns damage
 				return 2;
 			}
-			//normal damage against water/air
+			// normal damage against water/air
 			if (unitAffinity == Affinity.WATER || unitAffinity == Affinity.AIR) {
-				//returns damage
+				// returns damage
 				return 1;
 			}
-			//half damage against fire
+			// half damage against fire
 			if (unitAffinity == Affinity.FIRE) {
-				//returns damage
+				// returns damage
 				return 0.5;
 			}
-			
-		//checks if main element is earth
+
+			// checks if main element is earth
 		} else if (element == Affinity.EARTH) {
-			//double damage against water
+			// double damage against water
 			if (unitAffinity == Affinity.WATER) {
-				//returns damage
+				// returns damage
 				return 2;
 			}
-			//normal damage against fire/earth
+			// normal damage against fire/earth
 			if (unitAffinity == Affinity.FIRE || unitAffinity == Affinity.EARTH) {
-				//returns damage
-				return 1;	
-						}
-			//half damage against air
+				// returns damage
+				return 1;
+			}
+			// half damage against air
 			if (unitAffinity == Affinity.AIR) {
-				//returns damage
+				// returns damage
 				return 0.5;
 			}
-		
-		//checks if main element is fire
+
+			// checks if main element is fire
 		} else if (element == Affinity.FIRE) {
-			//double damage against air
+			// double damage against air
 			if (unitAffinity == Affinity.AIR) {
-				//returns damage
-				return 2;	
-						}
-			//normal damage against earth/fire
-			if (unitAffinity == Affinity.FIRE || unitAffinity == Affinity.EARTH) {
-				//returns damage
-				return 1;
-			}
-			//half damage against water
-			if (unitAffinity == Affinity.WATER) {
-				//returns damage
-				return 0.5;
-			} 
-		//checks if main element is water
-		} else if (element == Affinity.WATER) {
-			//double damage against fire
-			if (unitAffinity == Affinity.FIRE) {
-				//returns damage
+				// returns damage
 				return 2;
 			}
-			//normal damage against air/water
-			if (unitAffinity == Affinity.WATER || unitAffinity == Affinity.AIR) {
-				//returns damage
+			// normal damage against earth/fire
+			if (unitAffinity == Affinity.FIRE || unitAffinity == Affinity.EARTH) {
+				// returns damage
 				return 1;
 			}
-			//half damage against earth
+			// half damage against water
+			if (unitAffinity == Affinity.WATER) {
+				// returns damage
+				return 0.5;
+			}
+			// checks if main element is water
+		} else if (element == Affinity.WATER) {
+			// double damage against fire
+			if (unitAffinity == Affinity.FIRE) {
+				// returns damage
+				return 2;
+			}
+			// normal damage against air/water
+			if (unitAffinity == Affinity.WATER || unitAffinity == Affinity.AIR) {
+				// returns damage
+				return 1;
+			}
+			// half damage against earth
 			if (unitAffinity == Affinity.EARTH) {
-				//returns damage
+				// returns damage
 				return 0.5;
 			}
 		}
-		
-		//returns zero
+
+		// returns zero
 		return 0;
-		
-		
-		
+
 	}
-	
-	void update(){
-		//may or may not be needed here, depends on project development
+
+	void update() {
+		// may or may not be needed here, depends on project development
 	}
-	
+
 	void draw(Graphics g) {
 		g.setColor(Color.YELLOW);
-        g.fillRect(x, y, width, height);
-        
-        if (gotImage) {
-        	g.drawImage(image, x, y, width, height, null);
-        } else {
-        	g.setColor(Color.BLUE);
-        	g.fillRect(x, y, width, height);
-        }
+		g.fillRect(x, y, width, height);
+
+		if (gotImage) {
+			g.drawImage(image, x, y, width, height, null);
+		} else {
+			g.setColor(Color.BLUE);
+			g.fillRect(x, y, width, height);
+		}
 	}
-	
+
 	void loadImage(String imageFile) {
-	    if (needImage) {
-	        try {
-	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
-		    gotImage = true;
-	        } catch (Exception e) {
-	            
-	        }
-	        needImage = false;
-	    }
+		if (needImage) {
+			try {
+				image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+				gotImage = true;
+			} catch (Exception e) {
+
+			}
+			needImage = false;
+		}
 	}
-	
+
 }
